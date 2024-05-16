70e496bfd275140f3e8c81628c074568eef38356
Marco Antonio Castro Carreon <mcastrodevelopment@gmail.com>
2024-05-15 21:15:59 -0600

    feat: Added root endpoint to server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (_: Request, res: Response) => {
+    res.send('Server On in port: ' + PORT);
+})
 
 app.listen(PORT, () => console.log(`Server Listening in PORT :${PORT}`));
